476b3f5c54afe61dc759e92a38c2762f0b1b2db7
tantradnya <neeraj.adsul@gmail.com>
2019-12-19 22:34:29 +0530

    registartion form

--- app/forms.py
+++ app/forms.py
@@ -11,8 +11,9 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Enter Password', validators=[DataRequired()])
-    password2 = PasswordField('Confirm Password', validators=[DataRequired()], EqualTo('password'))
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
