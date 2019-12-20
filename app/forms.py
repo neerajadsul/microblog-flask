4cd8c05fed8308192be8e4752b40fc7d3032911d
tantradnya <neeraj.adsul@gmail.com>
2019-12-20 19:51:56 +0530

    form for profile editor

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms.validators import Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -25,3 +27,8 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Email already in use, use different one.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max-140)])
+    submit = SubmitField('Submit')
