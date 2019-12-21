70660f481b0093fd686cd0bc5d7025ad105582d9
tantradnya <neeraj.adsul@gmail.com>
2019-12-21 17:46:20 +0530

    methods for all followed posts

--- app/models.py
+++ app/models.py
@@ -50,6 +50,15 @@ class User(UserMixin, db.Model):
     def is_following(self, user):
         return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
+    def followed_posts(self):
+        posts_followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)
+            ).filter(
+                followers.c.follower_id == self.id
+                )
+        posts_own = Post.query.filter_by(user_id = self.id)
+        return posts_followed.union(posts_own).order_by(Post.timestamp.desc())
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
