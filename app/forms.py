a53b4bc7e8c2669b2c209db084a20adb6859140c
tantradnya <neeraj.adsul@gmail.com>
2019-12-21 21:53:10 +0530

    posting form, route and pages

--- app/forms.py
+++ app/forms.py
@@ -42,3 +42,7 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Username already in use, use different one.')
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,25 @@
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 from datetime import datetime
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=['GET','POST'])
+@app.route("/index", methods=['GET','POST'])
 @login_required
 def index():
-    user = {'username':'Neeraj'}
-    posts = [
-        {
-            'author':{'username':'Madhukar'},
-            'body':{'Flask is great for developing web apps'}
-        },
-        {
-            'author':{'username':'Sunita'},
-            'body':{'करडई ची भाजी is awesome!'}
-        }
-    ]
-    return render_template('index.html', title="Home Page", posts=posts)
-    # return render_template('index.html', title="", user=user)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live')
+        return redirect(url_for('index'))
+    
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title="Home Page", form=form, posts=posts)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -92,8 +89,47 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
 
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash(f'User {username} not found')
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You don\'t need to follow yourself.')
+        return redirect(url_for('user', username=username))
+
+    current_user.follow(user)
+    db.session.commit()
+    flash(f'You are now following {username}')
+    return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash(f'User {username} not found')
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You don\'t need to unfollow yourself.')
+        return redirect(url_for('user', username=username))
+
+    current_user.unfollow(user)
+    db.session.commit()
+    flash(f'You are not following {username} anymore')
+    return redirect(url_for('user', username=username))
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore',posts=posts)
+
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
